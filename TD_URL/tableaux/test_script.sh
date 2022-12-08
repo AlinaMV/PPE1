c483ce69e99f42aa73b57342e2a2041c3fa9e069
AlinaMV <miasnikovalina@gmail.com>
2022-12-08 10:44:20 +0100

    Mise à jour du script et des résultats de son travail

--- TD_URL/tableaux/test_script.sh
+++ TD_URL/tableaux/test_script.sh
@@ -4,24 +4,25 @@
 fichier_urls=$1 # le fichier d'URL en entrée
 fichier_tableau=$2 # le fichier HTML en sortie
 
+
 if [[ $# -ne 2 ]]
 then
 	echo "Ce programme demande exactement deux arguments."
 	exit
 fi
 
-mot=$(egrep "у(сыновл|дочер)ени")
-
 
 echo $fichier_urls;
 basename=$(basename -s .txt $fichier_urls)
 #basename -s retire le chemin, оставляет только название конечного файла. Option -s supprime l'extension
+mot="\bу|У(сыновл|дочер)ени.\b"
 
 echo "<html><body>" > $fichier_tableau
 echo "<h2>Tableau $basename :</h2>" >> $fichier_tableau
 echo "<br/>" >> $fichier_tableau
 echo "<table>" >> $fichier_tableau
-echo "<tr><th>ligne</th><th>code</th><th>URL</th><th>encodage</th></tr>" >> $fichier_tableau
+echo "<tr><th>ligne</th><th>code</th><th>URL</th><th>encodage</th><th>occurences</th></tr>" >> $fichier_tableau
+
 
 lineno=1;
 while read -r URL; do
@@ -61,11 +62,20 @@ while read -r URL; do
 		dump=""
 		charset=""
 	fi
+	
+	echo "$dump" > ../dumps-text/$basename-$lineno
+	
 
-	echo "<tr><td>$lineno</td><td>$code</td><td><a href=\"$URL\">$URL</a></td><td>$charset</td></tr>" >> $fichier_tableau
+	compteur=$(egrep -o $mot ../dumps-text/$basename-$lineno | wc -l)
+	
+	contexte=$(egrep -A2 -B2 $mot ../dumps-text/$basename-$lineno)
+	echo $contexte > ../contextes/$basename-$lineno
+	
+
+	echo "<tr><td>$lineno</td><td>$code</td><td><a href=\"$URL\">$URL</a></td><td>$charset</td><td>$compteur</td></tr>" >> $fichier_tableau
 	echo -e "\t--------------------------------"
 	lineno=$((lineno+1));
-	echo "$dump" > ../dumps-text/$basename-$lineno
+
 done < $fichier_urls
 echo "</table>" >> $fichier_tableau
 echo "</body></html>" >> $fichier_tableau
